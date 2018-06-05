7a8040c76d97e7b222bbeb03c65f1cc2ed8bb13d
yenato <yuantao.gu@mail.utoronto.ca>
2018-06-05 18:07:34 -0400

    Changed iterator object to list. Changed tests to test list

--- util/bot_collections.py
+++ util/bot_collections.py
@@ -1,47 +1,65 @@
-"""Iterator for a Linked List"""
-class LLIterator:
-    
-    """Creates a new LLIterator using an existing linked list node as the root"""
-    def __init__(self, node):
-        self.head = node
-        self.node = node
-    
-    def reset(self):
-        self.node = self.head
-    
-    """Returns false if this is the last value"""
-    def hasNext(self):
-        return self.node is not None and self.node.hasNext()
-    
-    """Returns the next value or None if there are no more values"""
-    def next(self):
-        if self.node is not None and self.node.hasNext():
-            self.node = self.node.getNext()
-            return self.node.getValue()
-        return None
-    
-    """Removes the current value"""
-    def delete(self):
-        if self.node is not None:
-            self.node.delete()
-            if self.node.hasPrev() == False:
-                self.head = self.node.tail
-                
-    """Inserts a new LLNode after the current one"""
-    def insert(self, value):
-        if self.head is None:
-            self.head = self.node = LLNode(value)
-        elif self.node is not None:
-            self.node.insertNext(value)
+"""
+Iterator for a Linked List
 
-    """Returns the value of the current node"""
-    def getValue(self):
-        return self.node.getValue()
-    
-    """Returns the current node"""
-    def getNode(self):
-        return self.node
+Single use iterator for linked list.
 
+"""
+class LList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+        
+    def begin(self):
+        return self.head
+    
+    def end(self):
+        return self.tail
+    
+    def isEmpty(self):
+        return self.length == 0
+    
+    """Adds a value to the front of the linked list"""
+    def push(self, newValue):
+        if self.head == None:
+            self.head = self.tail = LLNode(newValue)
+        else:
+            self.head = self.head.insertPrev(newValue)
+        self.length += 1
+        
+    """Adds a value to the end of the linked list"""
+    def pushEnd(self, newValue):
+        if self.head == None:
+            self.head = self.tail = LLNode(newValue)
+        else:
+            self.tail = self.tail.insertNext(newValue)
+        self.length += 1
+    
+    """Removes and returns the first value from the linked list"""
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        self.head = self.head.delete()
+        if self.length > 0:
+            self.length -= 1
+            if self.length == 0:
+                self.tail = None
+        return temp.getValue()
+    
+    """Removes and returns the last value from the linked list"""
+    def popEnd(self):
+        if self.length == 0:
+            return None
+        temp = self.tail
+        self.tail = self.tail.head
+        temp.delete()
+        if self.length > 0:
+            self.length -= 1
+            if self.length == 0:
+                self.head = None
+        return temp.getValue()
+    
 """Linked List Node"""
 class LLNode:
     
@@ -69,7 +87,7 @@ class LLNode:
     def setValue(self, newValue):
         self.value = newValue
     
-    """Interts the value as a new node after this one."""
+    """Interts the value as a new node after this one. Returns the new node"""
     def insertNext(self, newValue):
         temp = LLNode(newValue, head = self, tail = self.tail)
         if self.hasNext():
@@ -77,7 +95,7 @@ class LLNode:
         self.tail = temp
         return temp
         
-    """Interts the value as a new node before this one."""
+    """Interts the value as a new node before this one. Returns the new node"""
     def insertPrev(self, newValue):
         temp = LLNode(newValue, head = self.head, tail = self)
         if self.hasPrev():
@@ -85,20 +103,12 @@ class LLNode:
         self.head = temp
         return temp
         
-    """Interts the value as a new node on the end of the list"""
+    """Interts the value as a new node on the end of the list. Returns the new node."""
     def insertEnd(self, newValue):
         if self.hasNext:
             return self.tail.insertEnd(newValue)
         else:
             return self.insertNext(newValue)
-    
-    """Removes the next node in the list and returns that node"""
-    def deleteNext(self):
-        if self.hasNext():
-            temp = self.tail
-            self.tail = self.tail.delete()
-            return temp
-        return None
     
     """Removes this node from the list. Returns the next node."""
     def delete(self):
@@ -119,55 +129,89 @@ def testLinkedLists():
         assert rootNode.hasNext() == False
         assert rootNode.hasPrev() == False
         assert rootNode.getValue() == "root"
-        print("pass 1")
         
         node12 = rootNode.insertNext("123")
         assert rootNode.hasNext() == True
         assert node12.hasPrev() == True
-        print("pass 2")
         
         node6 = rootNode.insertNext("234")
         assert node12.getPrev().getValue() == "234"
         assert node6.getNext().getValue() == "123"
-        print("pass 3")
         
         delNode = node6.delete()
         assert delNode.getValue() == node12.getValue()
         assert rootNode.hasNext() == True
+        assert rootNode.getNext().getValue() == node12.getValue()
         assert node12.hasPrev() == True
-        print ("pass 4")
         
         assert rootNode.getNext().value == node12.value
         assert rootNode.value == node12.getPrev().value
-        print("pass 5")
         
         node9 = node12.insertPrev("345")
-        temp = node9.deleteNext()
-        assert node9.hasNext() == False
+        temp = node9.delete()
+        assert node12.getPrev().getValue() == rootNode.getValue()
         assert temp.value == node12.value
-        print("pass 6")
         
-        ## Testing iterator
+        ## Testing list
         
-        it = LLIterator(LLNode("root"))
+        llist = LList()
+        assert llist.begin() == None
+        assert llist.end() == None
+        assert llist.length == 0
+        assert llist.isEmpty() == True
         
-        it.delete()
-        it.insert("1")
-        it.insert("3")
-        it.insert("2")
-        assert it.getValue() == "1"
-        assert it.hasNext()
-        assert it.next() == "2"
-        it.delete()
-        assert it.next() == "3"
-        it.reset()
-        assert it.head is not None
-        assert it.next() == "3"
-        assert it.next() == None        
-        return True
-    
+        assert llist.pop() == None
+        assert llist.popEnd() == None
+        
+        llist.push("1")
+        assert llist.length == 1
+        assert llist.begin().getValue() == "1"
+        assert llist.end().getValue() == "1"
+        
+        llist.push("2")
+        assert llist.length == 2
+        assert llist.begin().getValue() == "2"
+        assert llist.end().getValue() == "1"
+        
+        llist.pushEnd("0")
+        assert llist.length == 3
+        assert llist.begin().getValue() == "2"
+        assert llist.end().getValue() == "0"
+        
+        poppedNode = llist.pop()
+        assert llist.length == 2
+        assert poppedNode == "2"
+        assert llist.begin().getValue() == "1"
+        assert llist.end().getValue() == "0"
+        
+        poppedNode = llist.popEnd()
+        assert llist.length == 1
+        assert poppedNode == "0"
+        assert llist.begin().getValue() == "1"
+        assert llist.end().getValue() == "1"
+        
+        poppedNode = llist.pop()
+        assert llist.length == 0
+        assert poppedNode == "1"
+        assert llist.begin() == None
+        assert llist.end() == None
+        
+        testList = LList()
+        testList.push(3)
+        testList.push(2)
+        testList.push(1)
+        testList.push(0)
+        
+        iterator = testList.begin()
+        c = 0
+        while iterator != None:
+            assert iterator.getValue() == c
+            c += 1
+            iterator = iterator.getNext()
+        
+        return "What horrific chain of events led to this outcome"
     except MemoryError:
-        return "wow how did this happen"
+        return "Wow how did this happen"
     
-print testLinkedLists()
+#print testLinkedLists()
     
