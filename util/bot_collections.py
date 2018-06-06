6e49ae249657a6831b68fefdb3794265329cc8b7
yenato <yuantao.gu@mail.utoronto.ca>
2018-06-06 00:31:16 -0400

    Moved linked list tests to tests folder

--- util/bot_collections.py
+++ util/bot_collections.py
@@ -1,9 +1,4 @@
-"""
-Iterator for a Linked List
-
-Single use iterator for linked list.
-
-"""
+"""Linked List"""
 class LList:
     def __init__(self):
         self.head = None
@@ -14,7 +9,7 @@ class LList:
         return self.head
     
     def end(self):
-        return self.tail
+        return None
     
     def isEmpty(self):
         return self.length == 0
@@ -118,100 +113,3 @@ class LLNode:
         if self.hasNext():
             self.tail.head = self.head
         return self.tail
-    
-    
-# You can run this in a run() if you need it for the tests 
-def testLinkedLists():
-    try:
-        ## Testing node
-        
-        rootNode = LLNode("root")
-        assert rootNode.hasNext() == False
-        assert rootNode.hasPrev() == False
-        assert rootNode.getValue() == "root"
-        
-        node12 = rootNode.insertNext("123")
-        assert rootNode.hasNext() == True
-        assert node12.hasPrev() == True
-        
-        node6 = rootNode.insertNext("234")
-        assert node12.getPrev().getValue() == "234"
-        assert node6.getNext().getValue() == "123"
-        
-        delNode = node6.delete()
-        assert delNode.getValue() == node12.getValue()
-        assert rootNode.hasNext() == True
-        assert rootNode.getNext().getValue() == node12.getValue()
-        assert node12.hasPrev() == True
-        
-        assert rootNode.getNext().value == node12.value
-        assert rootNode.value == node12.getPrev().value
-        
-        node9 = node12.insertPrev("345")
-        temp = node9.delete()
-        assert node12.getPrev().getValue() == rootNode.getValue()
-        assert temp.value == node12.value
-        
-        ## Testing list
-        
-        llist = LList()
-        assert llist.begin() == None
-        assert llist.end() == None
-        assert llist.length == 0
-        assert llist.isEmpty() == True
-        
-        assert llist.pop() == None
-        assert llist.popEnd() == None
-        
-        llist.push("1")
-        assert llist.length == 1
-        assert llist.begin().getValue() == "1"
-        assert llist.end().getValue() == "1"
-        
-        llist.push("2")
-        assert llist.length == 2
-        assert llist.begin().getValue() == "2"
-        assert llist.end().getValue() == "1"
-        
-        llist.pushEnd("0")
-        assert llist.length == 3
-        assert llist.begin().getValue() == "2"
-        assert llist.end().getValue() == "0"
-        
-        poppedNode = llist.pop()
-        assert llist.length == 2
-        assert poppedNode == "2"
-        assert llist.begin().getValue() == "1"
-        assert llist.end().getValue() == "0"
-        
-        poppedNode = llist.popEnd()
-        assert llist.length == 1
-        assert poppedNode == "0"
-        assert llist.begin().getValue() == "1"
-        assert llist.end().getValue() == "1"
-        
-        poppedNode = llist.pop()
-        assert llist.length == 0
-        assert poppedNode == "1"
-        assert llist.begin() == None
-        assert llist.end() == None
-        
-        testList = LList()
-        testList.push(3)
-        testList.push(2)
-        testList.push(1)
-        testList.push(0)
-        
-        iterator = testList.begin()
-        c = 0
-        while iterator != None:
-            assert iterator.getValue() == c
-            c += 1
-            iterator = iterator.getNext()
-        
-        return "What horrific chain of events led to this outcome"
-    except MemoryError:
-        return "Wow how did this happen"
-    
-#print testLinkedLists()
-    
