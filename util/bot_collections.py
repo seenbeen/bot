e56ea579813726ed874f0c2ec7cfcb0a674fe5cb
yenato <yuantao.gu@mail.utoronto.ca>
2018-06-07 03:28:59 -0400

    Changed begin/end to act like c++ iterators. Changed tests to fit new definitions (#6)

--- util/bot_collections.py
+++ util/bot_collections.py
@@ -1,58 +1,51 @@
 """Linked List"""
 class LList:
     def __init__(self):
-        self.head = None
-        self.tail = None
+        self.head = LLNode(None)
+        self.tail = self.head.insertNext(None)
         self.length = 0
         
     def begin(self):
-        return self.head
+        return self.head.next()
     
     def end(self):
-        return None
+        return self.tail
+    
+    def rbegin(self):
+        return self.tail.prev()
+    
+    def rend(self):
+        return self.head
     
     def isEmpty(self):
         return self.length == 0
     
     """Adds a value to the front of the linked list"""
     def push(self, newValue):
-        if self.head == None:
-            self.head = self.tail = LLNode(newValue)
-        else:
-            self.head = self.head.insertPrev(newValue)
+        self.head.insertNext(newValue)
         self.length += 1
         
     """Adds a value to the end of the linked list"""
     def pushEnd(self, newValue):
-        if self.head == None:
-            self.head = self.tail = LLNode(newValue)
-        else:
-            self.tail = self.tail.insertNext(newValue)
+        self.tail.insertPrev(newValue)
         self.length += 1
     
     """Removes and returns the first value from the linked list"""
     def pop(self):
         if self.length == 0:
-            return None
-        temp = self.head
-        self.head = self.head.delete()
-        if self.length > 0:
-            self.length -= 1
-            if self.length == 0:
-                self.tail = None
+            raise Exception('Tried to pop from empty list')
+        temp = self.head.next()
+        temp.delete()
+        self.length -= 1
         return temp.getValue()
     
     """Removes and returns the last value from the linked list"""
     def popEnd(self):
         if self.length == 0:
-            return None
-        temp = self.tail
-        self.tail = self.tail.head
+            raise Exception('Tried to pop from empty list')
+        temp = self.tail.prev()
         temp.delete()
-        if self.length > 0:
-            self.length -= 1
-            if self.length == 0:
-                self.head = None
+        self.length -= 1
         return temp.getValue()
     
 """Linked List Node"""
@@ -70,11 +63,17 @@ class LLNode:
     def hasPrev(self):
         return self.head is not None
     
-    def getNext(self):
-        return self.tail
+    def next(self):
+        if self.hasNext():
+            return self.tail
+        else:
+            raise StopIteration
     
-    def getPrev(self):
-        return self.head
+    def prev(self):
+        if self.hasPrev():
+            return self.head
+        else:
+            raise StopIteration
     
     def getValue(self):
         return self.value
