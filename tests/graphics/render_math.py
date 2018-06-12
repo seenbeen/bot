df3285e2db053059c9ee176588e14565dad415df
Shiyang Han <shiratori.seenbeen@gmail.com>
2018-06-11 20:42:04 -0700

    Made test more human-eye validation friendly. Swapped camera to use correct xform mat

--- tests/graphics/render_math.py
+++ tests/graphics/render_math.py
@@ -14,8 +14,10 @@ def run():
     def genStar(sides):
         return [map(int, rotatePt([100/3, 100][i%2], i*360/sides)) for i in range(sides)]
 
+    center = Vector2()
+
     camTransform = Transform()
-    camTransform.scale = Vector2(1.5, 1.5)
+    camTransform.scale = Vector2(0.75, 0.75)
     camDims = Vector2(800,600)
     vpDims = Vector2(800,600)
 
@@ -35,14 +37,30 @@ def run():
         starTransform.position = Vector2(*rotatePt(100, -starTransform.rotation))
         
         vMat = Mat33Util.getCamToViewportMatrix(camDims, vpDims)
-        cMat = camTransform.getMatrix()
+        cMat = camTransform.getInverseMatrix()
         mMat = starTransform.getMatrix()
         
         res = map(lambda v: vMat * cMat * mMat * v, vecStar)
         resPt = map(lambda v: map(int, [v.x, v.y]), res)
 
+        centerRes = vMat * cMat * center # note that mMat is just Identity for center
+        centerResPt = map(int, [centerRes.x, centerRes.y])
+        
         screen.fill((0, 0, 0))
         draw.polygon(screen, (0, 255, 0), resPt, 2)
+        
+        # we have to account for the camera's scale when showing paths
+        draw.circle(screen, (255, 0, 255), centerResPt, int(150/0.75), 1)
+        draw.circle(screen, (0, 255, 255), centerResPt, int(100/0.75), 1)
+
+        # origin
+        draw.circle(screen, (255, 0, 0), centerResPt, 5)
+
+        # camera cross-hair
+        crossHairSize = 10
+        draw.line(screen, (0, 255, 0), (400, 300 - crossHairSize), (400, 300 + crossHairSize), 2)
+        draw.line(screen, (0, 255, 0), (400 - crossHairSize, 300), (400 + crossHairSize, 300) , 2)
+        
         display.flip()
         
         myClock.tick(60)
