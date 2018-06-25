f6d7d802ae149219b014ed373c3956dcf3a356a5
Shiyang Han <shiratori.seenbeen@gmail.com>
2018-06-24 20:01:05 -0700

    Added EventQueueifier and modify test.py to include stack trace

---
+++ util/pattern/bot_eventqueue.py
@@ -0,0 +1,89 @@
+class EventQueue:
+    '''
+    On init:
+        - give class an event queue.
+
+    On specified methods, remap to a wrapped "pump" method which:
+        - instead, adds call bundle to event queue
+        - event queue is sorted based on the provided event comparator
+        - pump method calls cannot call other pump methods to
+          resolve themselves
+          Eg:
+              def myPumpMethod(self, you):
+                  you.yourPumpMethod()
+
+          This is illegal as calling pump surjects a pump call to which
+          when its actual execution occurs is undefined
+
+    pump is added as a method to class:
+        - goes through the entire queue and pumps all methods, calling them
+          as we go
+    '''
+    class MethodInfo:
+        def __init__(self, meth, name, args):
+            self.meth = meth
+            self.name = meth.__name__
+            self.__instance = args[0]
+
+            self.argsDict = {}
+            argCount = 1
+            argLen = len(args)
+            argNames = meth.im_func.func_code.co_varnames
+            while argCount < argLen:
+                self.argsDict[argNames[argCount]] = args[argCount]
+                argCount += 1
+
+        def call(self):
+            self.meth(self.__instance, **self.argsDict)
+
+
+    __ENQUEUEIFIED = "-ENQUEUEIFIED"
+    __QUEUE_KEY = "-EVENT_QUEUE"
+    __CALL_STACK_PUMP_METHOD = None
+    
+    @staticmethod
+    def __pumpifyMethod(method):
+        def pumpMethod(*args):
+            if EventQueue.__CALL_STACK_PUMP_METHOD:
+                raise Exception("Fatal: Attempting to surject pump method " +
+                                "'%s' during a pump flush of '%s'" %
+                                (method.__name__, EventQueue.__CALL_STACK_PUMP_METHOD))
+
+            bundle = EventQueue.MethodInfo(method, method.__name__, args)
+            args[0].__dict__[EventQueue.__QUEUE_KEY].append(bundle)
+        return pumpMethod
+
+    @staticmethod
+    def enQueueify(className, methods, eventComparator):
+        if EventQueue.__ENQUEUEIFIED in className.__dict__:
+            raise Exception("Attempting to enqueueify class '%s' twice!"%className.__name__)
+
+        oInit = className.__init__ if "__init__" in className.__dict__ else lambda self : None
+
+        for m in methods:
+            if type(m).__name__ not in ["instancemethod"]:
+                raise Exception("Attempting to enqueueify non-instance method " +
+                                "'%s' from class '%s'!"%(type(m).__name__, className.__name__)) 
+
+            mName = m.__name__
+            if mName not in className.__dict__:
+                raise Exception("Attempting to enqueuify non-existent instance method " +
+                                "'%s' in class '%s'!"%(m.__name__, className.__name__))
+
+            className.__dict__[mName] = EventQueue.__pumpifyMethod(m)
+
+        def __init__(self, *args):
+            self.__dict__[EventQueue.__QUEUE_KEY] = []
+            oInit(self, *args)
+
+        def pump(self):
+            evtQueue = self.__dict__[EventQueue.__QUEUE_KEY]
+            evtQueue.sort(cmp=eventComparator)
+            for x in evtQueue:
+                EventQueue.__CALL_STACK_PUMP_METHOD = x.name
+                x.call()
+            EventQueue.__CALL_STACK_PUMP_METHOD = None
+
+        className.__init__ = __init__
+        className.pump = pump
+        className.__dict__[EventQueue.__ENQUEUEIFIED] = True
